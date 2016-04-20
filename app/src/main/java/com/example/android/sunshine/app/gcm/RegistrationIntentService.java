9b7dd1ff0761a0fa1057be3fac0dd5d270272691
Rizky Eko <rizky.eko@elephanttalk.com>
2016-04-20 22:42:58 +0700

    Its WORK

--- app/src/main/java/com/example/android/sunshine/app/gcm/RegistrationIntentService.java
+++ app/src/main/java/com/example/android/sunshine/app/gcm/RegistrationIntentService.java
@@ -32,9 +32,13 @@ public class RegistrationIntentService extends IntentService {
                 // Initially this call goes out to the network to retrieve the token, subsequent calls
                 // are local.
                 InstanceID instanceID = InstanceID.getInstance(this);
-                String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
-                        GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
-                sendRegistrationToServer(token);
+                // TODO: gcm_default sender ID comes from the API console
+                String senderId = getString(R.string.gcm_defaultSenderId);
+                if ( senderId.length() != 0 ) {
+                    String token = instanceID.getToken(senderId,
+                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                    sendRegistrationToServer(token);
+                }
 
                 // You should store a boolean that indicates whether the generated token has been
                 // sent to your server. If the boolean is false, send the token to your server,
